bd12d9f39a742f3f5a6a56d03dcaf23031e9db71
Ajaydeep4 <ajaydeep44444@gmail.com>
2021-08-09 00:43:48 +0530

    stats code

---
+++ stats-unit-Ajaydeep4/Statistics.Test/StatsComputerLibMaxUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using StatisticsLib;
+using AlertsLib;
+
+using System.Collections.Generic;
+namespace StatisticsLib.Test
+{
+    public class StatsComputerLibMaxUnitTest
+    {
+        [Fact]
+        public void ReportsMax()
+        {
+            var statsComputer = new StatsComputer();
+            var max = statsComputer.GetMax(
+                new List<double> { 1.5, 8.9, 3.2, 4.5 });
+            float epsilon = 0.001F;
+            Assert.True(Math.Abs(max - 8.9) <= epsilon);
+        }
+        [Fact]
+        public void ReportsNaNForEmptyInput()
+        {
+            var statsComputer = new StatsComputer();
+            var max = statsComputer.GetMax(
+                new List<double> { });
+            Assert.True(Double.NaN.Equals(max));
+        }
+        [Fact]
+        public void ReportsNaNIfInputContainsNaN()
+        {
+            var statsComputer = new StatsComputer();
+            var max = statsComputer.GetMax(
+                new List<double> { 1.5, 8.9, double.NaN, 4.5 });
+            Assert.True(Double.NaN.Equals(max));
+        }
+
+    }
+
+}
--- stats-unit-Ajaydeep4/Statistics/StatsComputer.cs
+++ stats-unit-Ajaydeep4/Statistics/StatsComputer.cs
@@ -5,26 +5,53 @@ namespace StatisticsLib
 {   
     public class StatsComputer
     {
-        public StatsDataModel CalculateStatistics(List<double> numbers)
+        public double GetMax(List<double> numbers)
         {
-            double max, min, average;
+            double max;
             if (numbers.Count == 0)
-                return new StatsDataModel();
+                return Double.NaN;
             max = numbers[0];
-            min = numbers[0];
-            average = 0;
-            foreach(float number in numbers)
+            foreach (float number in numbers)
             {
                 if (Double.NaN.Equals(number))
-                    return new StatsDataModel();
+                    return Double.NaN;
                 if (number > max)
                     max = number;
+            }
+            return max;
+        }
+        public double GetMin(List<double> numbers)
+        {
+            double min;
+            if (numbers.Count == 0)
+                return Double.NaN;
+            min = numbers[0];
+            foreach (float number in numbers)
+            {
+                if (Double.NaN.Equals(number))
+                    return Double.NaN;
                 if (number < min)
                     min = number;
-                average += number;
             }
-            average /= numbers.Count;
-            return new StatsDataModel(average, max, min);
+            return min;
+        }
+        public double GetAverage(List<double> numbers)
+        {
+            double avg=0;
+            if (numbers.Count == 0)
+                return Double.NaN;
+            foreach (float number in numbers)
+            {
+                if (Double.NaN.Equals(number))
+                    return Double.NaN;
+                avg += number;
+            }
+            return avg/numbers.Count;
+        }
+
+        public StatsDataModel CalculateStatistics(List<double> numbers)
+        {
+            return new StatsDataModel(GetAverage(numbers), GetMax(numbers), GetMin(numbers));
         }
     }   
 }
