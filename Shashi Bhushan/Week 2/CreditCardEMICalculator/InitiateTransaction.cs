3d0f8f85c492495f09d74065973bef354bf2db6b
Shashi Bhushan Sharma <shashi.sharma@publicissapient.com>
2021-08-23 14:25:55 +0530

    Adding More Assignment

---
+++ Shashi Bhushan/Week 2/CreditCardEMICalculator/InitiateTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreditCardEMICalculator
+{
+    class InitiateTransaction
+    {
+        public static void DisplayTransactionDetails(TransactionDetails obj)
+        {
+            Console.WriteLine("Monthly EMI              :  Rs.   {0}", obj.emiPayable);
+            Console.WriteLine("Total Interest Amount    :  Rs.   {0}", obj.totalInterestAmount);
+            Console.WriteLine("Total Amount Payable     :  Rs.   {0}", obj.totalAmountPayable);
+        }
+        static void InputTransactionDetails()
+        {
+            var principalAmt = Convert.ToDouble(Console.ReadLine());
+            var tenure = Convert.ToInt32(Console.ReadLine());
+            var roi = Convert.ToDouble(Console.ReadLine());
+
+            TransactionDetails transactionObj = new TransactionDetails(principalAmt, tenure, roi);
+            
+            TransactionCalculationUtility.CalculationUtility(transactionObj);
+
+            InitiateTransaction.DisplayTransactionDetails(transactionObj);
+        }
+        static void Main(string[] args)
+        {
+            InitiateTransaction.InputTransactionDetails();
+        }
+    }
+}
---
+++ Shashi Bhushan/Week 2/CreditCardEMICalculator/TransactionDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditCardEMICalculator
+{
+    public class TransactionDetails
+    {
+        public double TransactionAmount;
+        public int Tenure;
+        public double rateOfInterest;
+        public double emiPayable;
+        public double totalInterestAmount;
+        public double totalAmountPayable;
+
+        public TransactionDetails(double transactionAmt, int tenure, double roi)
+        {
+            TransactionAmount = transactionAmt;
+            Tenure = tenure;
+            rateOfInterest = roi;
+        }
+    }
+}
