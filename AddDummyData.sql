c51d11eff4417811e6d65c7ad20e5739dc1a733c
HemaAla <31954901+HemaAla@users.noreply.github.com>
2021-09-03 19:41:52 +0530

    Add files via upload

---
+++ AddDummyData.sql
@@ -0,0 +1,34 @@
+-- SELECT *
+-- FROM [dbo].[Users];
+
+
+-- -- INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('ala.hema1718@gmail.com','admin');
+
+
+
+-- -- SELECT *
+-- -- FROM [dbo].[Users]
+
+-- -- UPDATE [dbo].[Users]
+-- -- SET [EmailId] = 'alahema@publicisgroupe.net'
+
+-- -- SELECT *
+-- -- FROM [dbo].[Users]
+
+
+-- INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('shashi@publicisgroupe.net','admin');
+
+-- SELECT *
+-- FROM [dbo].[Users];
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('ala.hema1718@gmail.com','admin');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('shashi@publicisgroupe.net','admin');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('piyrai1@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('sagjaisw@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('shrkulsh3@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('nikyadav@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('avayadav@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('mithirna@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('dhirana@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('avagupta@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('kaumella@publicisgroupe.net','risk analyst');
+INSERT INTO [dbo].[Users] ( [EmailId],[UserRole])VALUES('aja@publicisgroupe.net','risk analyst');
